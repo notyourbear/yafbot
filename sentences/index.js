fba9f1faeed634ecb521143e45bdbd0bace8f167
notyourbear <pracenberg@gmail.com>
2016-08-08 09:33:33 -0700

    removed testing code

--- sentences/index.js
+++ sentences/index.js
@@ -1,10 +1,10 @@
 module.exports = [
-  // require('./destruction_item'),
-  // require('./item_destruction'),
-  // require('./arrival_item'),
-  // require('./item_arrival'),
-  // require('./destruction_place_item'),
-  // require('./outsider_arrival_potential'),
-  // require('./animosity_truePower'),
+  require('./destruction_item'),
+  require('./item_destruction'),
+  require('./arrival_item'),
+  require('./item_arrival'),
+  require('./destruction_place_item'),
+  require('./outsider_arrival_potential'),
+  require('./animosity_truePower'),
   require('./truePower_animosity')
 ];
