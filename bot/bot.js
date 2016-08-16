2c1a1d19e2cfe7f4ff5ecb80e8d58fdec63cc050
notyourbear <pracenberg@gmail.com>
2016-08-15 18:36:52 -0700

    long tweet support added

--- bot/bot.js
+++ bot/bot.js
@@ -9,8 +9,28 @@ var Bot = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
-Bot.tweet = (text) => {
-  console.log(text)
+Bot.tweet = (text, self) => {
+  var part1, part2;
+  if(text.length >= 140){
+    part1 = text.slice(0, 140)
+    part2 = text.slice(140)
+  } else {
+    part1 = text
+  }
+
+  if(self !== true){
+    console.log(part1)
+  } else {
+    console.log('@young_adult_fic' + part1)
+  }
+
+  if(_.isString(part2)){
+    Bot.tweet(part2, true)
+  }
+  // what needs to happen?
+  // if its larger that 140 it needs to be cut up into parts
+  // the first part will be tweeted,
+  // the second will be sent through and bot tweeted with @...
   // this.post('statuses/update', {status: text}, function(err, data, response){
   //   if (err) {
   //     console.log(err);
