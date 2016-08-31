52f42374854fa4bf77b16c4dd7aa27ab2e5cfb4b
notyourbear <pracenberg@gmail.com>
2016-08-30 18:36:21 -0700

    remove unnecessary

--- sentences/different_truePlace.js
+++
@@ -1,9 +0,0 @@
-_ = require('underscore');
-
-module.exports = [
-  _.sample(['schema/util/person_age_job', 'schema/util/age_job', 'schema/util/person_age'])
-  'schema/setUp/different',
-  'schema/util/period'
-];
-
-// She must find her true place...
